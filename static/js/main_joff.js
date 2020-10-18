455708b09d949d7d3440ce62a399866a4fbb9969
jshao109 <shao.j109@gmail.com>
2020-10-18 06:19:04 -0400

    attempted to merge front-end

--- static/js/main_joff.js
+++ static/js/main_joff.js
@@ -1,16 +1,16 @@
 var containers = document.querySelectorAll(".container");
 
 class item {
-  constructor(name, priority, start, end, duration, due) {
-    this.createDiv(name, priority, start, end, duration, due);
+  constructor(name, priority, duration) {
+    this.createDiv(name, priority, duration);
   }
 
-  createDiv(name, priority, start, end, duration, due) {
+  createDiv(name, priority, duration) {
     // assigns name and time for task to task box
     let input = document.createElement("input");
     input.type = "text";
-    var taskTime = start.getHours() + ":" + start.getMinutes() + " - " end.getHours() + ":" + end.getMinutes();
-    input.value = name; + " ~ " + taskTime;
+    // var taskTime = start.getHours() + ":" + start.getMinutes() + " - " end.getHours() + ":" + end.getMinutes();
+    input.value = name; //+ " ~ " + taskTime;
 
     // prevents task from being edited
     input.disabled = true;
@@ -31,10 +31,10 @@ class item {
     var adjheight = num.toString() + "px";
     input.style.height = adjheight;
 
-    // creates due date text
+    /* creates due date text
     let dueDate = document.createElement("h4");
     dueDate.classList.add("dd");
-    dueDate.value = "Due Date/Time: <br />" + due.getMonth() + " " + due.getDay() + ", " + due.getYear();
+    dueDate.value = "Due Date/Time: <br />" + due.getMonth() + " " + due.getDay() + ", " + due.getYear(); */
 
     // creates a new box for task
     let taskBox = document.createElement("div");
@@ -42,7 +42,7 @@ class item {
 
     // puts actual task in the box
     taskBox.appendChild(input);
-    taskBox.appendChild(dueDate);
+    //taskBox.appendChild(dueDate);
 
     // decides whether the task box goes in the morning or afternoon
     if (priority === "flex1" || priority === "fixed1") {
